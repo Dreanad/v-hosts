f73ba6e1d9f4556093d8ef558a4211be658e6e0e
Louis-Etienne <louis-etienne@leuilliot.fr>
2016-04-24 00:22:24 +0200

    WIP v-hosts

---
+++ app/uninstall.sh
@@ -0,0 +1,12 @@
+# v-hosts uninstall require root access
+if [ "$(whoami)" != "root" ]
+  then
+    echo "If you want uninstall V-Hosts, please run the uninstall with root access"
+    exit
+fi
+
+sudo rm -Rf /etc/v-hosts
+sudo rm /usr/bin/v-hosts
+sudo rm -- "$0"
+
+echo "V-Hosts is now removed !"
---
+++ install.sh
@@ -0,0 +1,12 @@
+# v-hosts install require root access
+if [ "$(whoami)" != "root" ]
+  then
+    echo "If you want install V-Hosts, please run the install with root access"
+    exit
+fi
+
+sudo mkdir /etc/v-hosts
+sudo cp app/conf/* /etc/v-hosts
+sudo cp app/v-hosts /usr/bin/
+
+echo "V-Hosts is now installed !"
